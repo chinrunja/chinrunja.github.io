20907a47615e85a5760b7a68736d7eda21d090cf
chinrunja <76439159+chinrunja@users.noreply.github.com>
2020-12-25 20:18:35 +0800

    Create index.js
    Add index JavaScript

---
+++ script/index.js
@@ -0,0 +1,13 @@
+// Button for background color change script
+const btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function bgChange() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.onclick = bgChange;
